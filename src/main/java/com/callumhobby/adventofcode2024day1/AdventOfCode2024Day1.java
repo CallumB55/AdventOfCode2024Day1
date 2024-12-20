69a6c7a6c8db8d17136c8ef2a18d556d15a3c835
callum <callum.binns@insitetrack.com>
2024-12-20 16:44:20 +0000

    saving work in main file

--- src/main/java/com/callumhobby/adventofcode2024day1/AdventOfCode2024Day1.java
+++ src/main/java/com/callumhobby/adventofcode2024day1/AdventOfCode2024Day1.java
@@ -3,6 +3,11 @@
  */
 
 package com.callumhobby.adventofcode2024day1;
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.TreeSet;
+import java.util.TreeMap;
 
 /**
  *
@@ -11,6 +16,23 @@ package com.callumhobby.adventofcode2024day1;
 public class AdventOfCode2024Day1 {
 
     public static void main(String[] args) {
+        File file = new File("src\\main\\java\\input.txt");
+        TreeSet<Integer> right = new TreeSet<>(); //use of bsts results in more lines but a more interesting solution
+        TreeMap<Integer, Integer> lists = new TreeMap<>();
+        try{
+            Scanner scan = new Scanner(file);
+            while (scan.hasNextLine()) {
+                String[] ln = scan.nextLine().split("   ");
+                right.add(Integer.valueOf(ln[1]));
+                lists.put(Integer.valueOf(ln[0]),null);
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        
+        
+
         System.out.println("Hello World!");
     }
 }
