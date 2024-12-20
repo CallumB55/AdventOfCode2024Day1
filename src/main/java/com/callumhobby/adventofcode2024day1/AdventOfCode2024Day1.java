c2f92ac293b8e900e64279dc073eb9f2f70a6693
callum <callum.binns@insitetrack.com>
2024-12-20 17:21:57 +0000

    Final algorithm with input split bug

--- src/main/java/com/callumhobby/adventofcode2024day1/AdventOfCode2024Day1.java
+++ src/main/java/com/callumhobby/adventofcode2024day1/AdventOfCode2024Day1.java
@@ -7,7 +7,7 @@ import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.util.TreeSet;
-import java.util.TreeMap;
+import java.util.Iterator;
 
 /**
  *
@@ -17,22 +17,32 @@ public class AdventOfCode2024Day1 {
 
     public static void main(String[] args) {
         File file = new File("src\\main\\java\\input.txt");
-        TreeSet<Integer> right = new TreeSet<>(); //use of bsts results in more lines but a more interesting solution
-        TreeMap<Integer, Integer> lists = new TreeMap<>();
+        TreeSet<Integer> left = new TreeSet<>();
+        TreeSet<Integer> right = new TreeSet<>(); 
         try{
             Scanner scan = new Scanner(file);
             while (scan.hasNextLine()) {
                 String[] ln = scan.nextLine().split("   ");
+                left.add(Integer.valueOf(ln[0]));
                 right.add(Integer.valueOf(ln[1]));
-                lists.put(Integer.valueOf(ln[0]),null);
+                
             }
         }
         catch (FileNotFoundException e){
             System.out.println(e.getMessage());
         }
-        
+        Iterator<Integer> leftIt = left.iterator();
+        Iterator<Integer> rightIt = right.iterator();
+        Integer totalDistance = 0;
+        Integer iterationCount = 0;
+        while (leftIt.hasNext() && rightIt.hasNext()) {
+            totalDistance += Math.abs(leftIt.next()-rightIt.next());
+            iterationCount ++;
+        }
         
 
-        System.out.println("Hello World!");
+        System.out.println(totalDistance);
+        System.out.println(iterationCount);
+        System.out.println(Integer.toString(right.size()));
     }
 }
